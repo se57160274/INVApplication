b89cf8301e5e4ee16a0bbe50107b645614826d2a
natchapon27 <natchapon.ang@gmail.com>
2017-05-08 20:02:32 +0700

    borrow

--- app/src/main/java/com/example/home/invapplication/MainFragment.java
+++ app/src/main/java/com/example/home/invapplication/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -30,7 +31,21 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_main, container, false);
-        //ImageView borrow = (ImageView)v.findViewById(R.id.borrow);
+        ImageView borrow = (ImageView)v.findViewById(R.id.borrow);
+        borrow.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                ProfileFragment fragment = new ProfileFragment();
+                android.support.v4.app.FragmentTransaction fragmentTransaction =
+                        getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_borrow, fragment);
+                fragmentTransaction.commit();
+            }
+
+
+        });
 
 
        // View v = inflater.inflate(R.layout.fragment_main, container, false);
